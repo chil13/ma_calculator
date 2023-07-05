3924184e5f1312b31c1be90011d4840c4853be6b
chil13 <matsuo.mbc6@gmail.com>
2023-07-05 13:45:55 +0900

    rewrite main

--- ma_calculator.py
+++ ma_calculator.py
@@ -1,55 +1,51 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-from openpyxl import load_workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
-#import plotly.graph_objects as go
 
 
 
-#st.title('MA Calculator')
+img = Image.open('header.png')
+st.image(img, use_column_width=True)
 
-def ma():
-    img = Image.open('header.png')
-    st.image(img, use_column_width=True)
+uploaded_file = st.sidebar.file_uploader("ファイル選択", type="csv")
 
-    uploaded_file = st.sidebar.file_uploader("ファイル選択", type="csv")
+df = pd.DataFrame()
+merged_table = pd.DataFrame()
 
-    df = pd.DataFrame()
-    merged_table = pd.DataFrame()
-
-    expander1 = st.expander('読込ファイル')
-    if uploaded_file is not None:
-        df = pd.DataFrame(pd.read_csv(uploaded_file, encoding='ms932'))
-        expander1.write(df)
-    else:
-        expander1.write('CSVファイルをアップロードしてください')
+expander1 = st.expander('読込ファイル')
+if uploaded_file is not None:
+    df = pd.DataFrame(pd.read_csv(uploaded_file, encoding='ms932'))
+    expander1.write(df)
+else:
+    expander1.write('CSVファイルをアップロードしてください')
 
 
-    #クロス軸のプルダウン作成
-    q_list=[]
-    colmuns = list(df.columns)
+#クロス軸のプルダウン作成
+q_list=[]
+colmuns = list(df.columns)
 
-    for column in colmuns:
-        first_word = column.split()[0]
-        if first_word[0] == "Q":
-            if first_word not in q_list:
-                q_list.append(first_word)
-    cross1 = st.sidebar.selectbox(
-        "クロス軸",
-        q_list
-    )
-    cross2 = st.sidebar.selectbox(
-        "設問",
-        q_list
-    )
+for column in colmuns:
+    first_word = column.split()[0]
+    if first_word[0] == "Q":
+        if first_word not in q_list:
+            q_list.append(first_word)
+cross1 = ""
+cross1 = st.sidebar.selectbox(
+    "クロス軸",
+    q_list
+)
+cross2 = st.sidebar.selectbox(
+    "設問",
+    q_list
+)
 
+def pivot_calc(cross1, cross2):
     cross1_key = str(cross1) + " "
     cross2_key = str(cross2) + " "
 
-    #ピヴォットの計算
     df_mt = pd.DataFrame()
-    df_mt_p = pd.DataFrame()
+    #ピヴォットの計算
     if cross1 != cross2:
         for c in colmuns:
             if c.startswith(cross1_key) == True:
@@ -68,59 +64,92 @@ def ma():
         botai = df.pivot_table(index=[c1], values=colmuns[0], aggfunc='count')
         df_mt = pd.DataFrame(merged_table)
         df_mt['母体数'] = botai
+    
+    return df_mt
 
-        df_mt_p = df_mt.copy()
-        df_mt_p.iloc[:, :] = df_mt_p.iloc[:, :].div(df_mt_p.iloc[:, -1], axis=0)
+df_mt = pivot_calc(cross1, cross2)
 
-    #タブ作成
-    tab1,tab2 = st.tabs(["ピボット","グラフ"])
+if len(df_mt) != 0:
+    df_mt_p = df_mt.copy()
+    df_mt_p.iloc[:, :] = df_mt_p.iloc[:, :].div(df_mt_p.iloc[:, -1], axis=0)
+else:
+    df_mt_p = pd.DataFrame()
 
-    with tab1:
-        #結果の表示
-        st.info('人数')
-        st.dataframe(df_mt)
+#タブ作成
+tab1,tab2 = st.tabs(["ピボット","グラフ"])
 
-        st.info('割合')
-        st.dataframe(df_mt_p.style.format('{:.2%}'))
+with tab1:
+    #結果の表示
+    st.info('人数')
+    st.dataframe(df_mt)
 
-    with tab2:
-        if len(df_mt_p) != 0:
-            #グラフの種類選択
-            stock = st.radio(label='グラフの種類選択',
-                            options=('棒グラフ', '面グラフ', '折れ線グラフ'),
-                            index=0,
-                            horizontal=True)
-            #グラフ用df用意
-            df_mt_p_glaph = df_mt_p.drop('母体数', axis=1)
-            if stock == "棒グラフ":
-                st.bar_chart(df_mt_p_glaph.style.format('{:.2%}'))
-            elif stock == "面グラフ":
-                st.area_chart(df_mt_p_glaph.style.format('{:.2%}'))
-            elif stock == "折れ線グラフ":
-                st.line_chart(df_mt_p_glaph.style.format('{:.2%}'))
+    st.info('割合')
+    st.dataframe(df_mt_p.style.format('{:.2%}'))
 
-    #DL設定
-    expander2 = st.sidebar.expander('ダウロード')
+with tab2:
+    if len(df_mt_p) != 0:
+        #グラフの種類選択
+        stock = st.radio(label='グラフの種類選択',
+                        options=('棒グラフ', '面グラフ', '折れ線グラフ'),
+                        index=0,
+                        horizontal=True)
+        #グラフ用df用意
+        df_mt_p_glaph = df_mt_p.drop('母体数', axis=1)
+        if stock == "棒グラフ":
+            st.bar_chart(df_mt_p_glaph.style.format('{:.2%}'))
+        elif stock == "面グラフ":
+            st.area_chart(df_mt_p_glaph.style.format('{:.2%}'))
+        elif stock == "折れ線グラフ":
+            st.line_chart(df_mt_p_glaph.style.format('{:.2%}'))
+
+#DL設定
+text = ""
+out_cross = []
+
+expander2 = st.sidebar.expander('ダウロード')
+if len(q_list) != 0:
     text = expander2.text_input("出力ファイル名")
-    button = expander2.button("保存")
-    file_name = text + '.xlsx'
+    file_name = text + '.csv'
+    #out_cross = expander2.multiselect(label="保存する設問  （※クロス軸：" + cross1 + "）", options=q_list)
+    out_cross = expander2.multiselect(label="保存する設問(※クロス軸：" + cross1 +")", options=q_list)
 
-    if button:
+    #保存用データ格納
+    append_pivot =[]
+
+    if expander2.button("データ作成"):
         if text != "":
-            try:
-                with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
-                    df_mt.to_excel(writer, sheet_name=cross2, index=True, header=True)
-                with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                    df_mt_p.to_excel(writer, sheet_name=cross2, index=True, header=True, startrow=len(df_mt)+2)
-                expander2.markdown('<span style="color:green">保存しました</span>', unsafe_allow_html=True)
-            except FileNotFoundError:
-                with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
-                    df_mt.to_excel(writer, sheet_name=cross2, index=True, header=True)
-                    df_mt_p.to_excel(writer, sheet_name=cross2, index=True, header=True, startrow=len(df_mt)+2)
-                expander2.markdown('<span style="color:green">保存しました</span>', unsafe_allow_html=True)
-            except PermissionError:
-                expander2.markdown('<span style="color:red">※指定ファイルを閉じてください</span>', unsafe_allow_html=True)
+            for oc in out_cross:
+                out_df_mt = pivot_calc(cross1, oc)
+                out_df_mt_p = out_df_mt.copy()
+                out_df_mt_p.iloc[:, :] = out_df_mt_p.iloc[:, :].div(out_df_mt_p.iloc[:, -1], axis=0)
+
+                # 空行を追加
+                append_pivot.append([oc,'=========================================================================================='])
+
+                # 空行を追加
+                append_pivot.append(['▼人数▼',])
+                # DataFrame1をExcelシートに書き込む
+                for row in dataframe_to_rows(out_df_mt, index=True, header=True):
+                    append_pivot.append(row)
+                
+                # 空行を追加
+                append_pivot.append([])
+
+                # 空行を追加
+                append_pivot.append(['▼割合▼',])
+                # DataFrame2をExcelシートに書き込む
+                for row in dataframe_to_rows(out_df_mt_p, index=True, header=True):
+                    append_pivot.append(row)
+
+                # 空行を追加
+                append_pivot.append([])
+
+            
+            csv = pd.DataFrame(append_pivot).to_csv(index=False, header=False).encode('ms932')
+            expander2.download_button(  label='Data Download', 
+                                        data=csv, 
+                                        file_name=text + '.csv',
+                                        mime='text/csv'
+                                        )
         else:
             expander2.markdown('<span style="color:red">※ファイル名を入力してください</span>', unsafe_allow_html=True)
-if __name__ == "__main__": 
-    ma()
